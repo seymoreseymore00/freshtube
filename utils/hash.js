5ee9862038fd405885e2a663533a87785ec5299d
seymoreseymore00 <seymorecbanton@inbox.lv>
2026-06-28 03:02:09 +0200

    Refactor sha256 function to create TextEncoder inline

--- utils/hash.js
+++ utils/hash.js
@@ -1,8 +1,7 @@
 export async function sha256(text) {
-  const enc = new TextEncoder();
   const buf = await crypto.subtle.digest(
     "SHA-256",
-    enc.encode(text)
+    new TextEncoder().encode(text)
   );
 
   return [...new Uint8Array(buf)]
