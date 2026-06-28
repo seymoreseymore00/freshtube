26d737053be641adefd8ed67f238ec6f2bbb5318
seymoreseymore00 <seymorecbanton@inbox.lv>
2026-06-28 03:03:03 +0200

    Update playlists.js

--- functions/api/playlists.js
+++ functions/api/playlists.js
@@ -1,5 +1,9 @@
 export async function onRequestGet({ env }) {
-  const db = env["freshtube-db"];
-  const { results } = await db.prepare("SELECT * FROM playlists").all();
+  const db = env.DB;
+
+  const { results } = await db.prepare(
+    "SELECT * FROM playlists ORDER BY id DESC"
+  ).all();
+
   return Response.json(results);
 }
